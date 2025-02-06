412751b69e5323c022ff28edaaa7626e78ff917b
Khari Jarrett <khari@viam.com>
2025-02-06 15:19:05 -0500

    fix their crop method for me

--- tracker/classify.go
+++ tracker/classify.go
@@ -15,7 +15,6 @@ import (
 // method will take a slice of tracks and return a slice of tracks.
 // The only difference is that the track will now include the detection classification
 func classifyTracks(ctx context.Context, tracks []*track, img image.Image, classifier vision.Service, logger logging.Logger) []*track {
-
 	if classifier == nil {
 		return tracks
 	}
@@ -39,35 +38,8 @@ func cropImageFromDet(img image.Image, det objdet.Detection) image.Image {
 	if bb.Max.X == 0 || bb.Max.Y == 0 {
 		return img
 	}
-	// Get image bounds
-	bounds := img.Bounds()
-	width := bounds.Max.X - bounds.Min.X
-	height := bounds.Max.Y - bounds.Min.Y
 
-	// Convert relative coordinates to absolute pixels
-	x1 := bounds.Min.X + int(float64(bb.Min.X)*float64(width))
-	y1 := bounds.Min.Y + int(float64(bb.Min.Y)*float64(height))
-	x2 := bounds.Min.X + int(float64(bb.Max.X)*float64(width))
-	y2 := bounds.Min.Y + int(float64(bb.Max.X)*float64(height))
-
-	// Create cropping rectangle
-	rect := image.Rect(x1, y1, x2, y2)
-	croppedImg := image.NewRGBA(image.Rect(0, 0, rect.Dx(), rect.Dy()))
-	draw.Draw(croppedImg, croppedImg.Bounds(), img, rect.Min, draw.Src)
+	croppedImg := image.NewRGBA(image.Rect(0, 0, bb.Dx(), bb.Dy()))
+	draw.Draw(croppedImg, croppedImg.Bounds(), img, bb.Min, draw.Src)
 	return croppedImg
 }
-
-/*
-func cropImageFromDet2(img image.Image, det objdet.Detection) image.Image {
-	lazyImg, ok := img.(*rimage.LazyEncodedImage)
-	if ok {
-		im := lazyImg.DecodedImage()
-		cropped := im.(interface {
-			SubImage(r image.Rectangle) image.Image
-		}).SubImage(*det.BoundingBox())
-		return cropped
-	}
-
-	return nil
-}
-*/
